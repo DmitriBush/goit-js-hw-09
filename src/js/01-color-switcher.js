61081fd7900f87ba7b5fe92f0cde324696fadb2c
DmitriiBush <dmitriybush77@gmail.com>
2023-02-13 22:31:51 +0100

    first commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+let timer = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    console.log(body.style.backgroundColor);
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+});
+
+// startBtn.addEventListener('click', changeColor);
+
+// function changeColor() {
+//   timer = setInterval(() => {
+//     body.style.backgroundColor = getRandomHexColor();
+//     console.log(body.style.backgroundColor);
+//   }, 1000);
+//   startBtn.setAttribute('disabled', '');
+// }
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  startBtn.removeAttribute('disabled');
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onPromiseGenerate);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onPromiseGenerate(event) {
+  event.preventDefault();
+  const formElement = event.currentTarget.elements;
+  const delayFirst = formElement.delay.value;
+  const step = formElement.step.value;
+  const amount = formElement.amount.value;
+
+  for (let i = 1; i <= amount; i += 1) {
+    const delay = +delayFirst + +step * (i - 1);
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
